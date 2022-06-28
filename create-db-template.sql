88bcb9d9874335699c1e714057e0ed5ddea44f24
DanioM0204 <danilo_mbezerra@hotmail.com>
2022-06-28 10:50:23 -0300

    Atualização ORM em NodeJs

--- create-db-template.sql
+++ create-db-template.sql
@@ -1,9 +1,9 @@
 -- Active: 1656334562351@@127.0.0.1@3306
 GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost';
 
-SHOW DATABASES;
+USE alura
+SHOW TABLES
+DESCRIBE pessoas;
 
-CREATE DATABASE alura;
-
-
-
+INSERT INTO pessoas (nome, ativo, email, role, createdAt, updatedAt) VALUES ("Carla Gomes", 1, "carla@carla.com", "estudante", NOW(), NOW());
+SELECT * FROM pessoas;

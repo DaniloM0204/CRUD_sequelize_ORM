23faca91ffa8a20a74cd805b941072f45e66b7fe
DanioM0204 <danilo_mbezerra@hotmail.com>
2022-07-15 13:55:18 -0300

    operações em dois modelos + transações

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -21,6 +21,7 @@ router //Matriculas
 .put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
 .post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
 .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+.post('/pessoas/:estudanteId/cancela', PessoaController.cancelaPessoa)
 .delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
 
 module.exports = router
